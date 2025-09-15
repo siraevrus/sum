4176e26145a29b3a0bd9fdf75644735e296f3e6e
Ruslan <ruslan@onza.me>
2025-09-15 22:47:35 +0300

    ProductInTransitResource: change main info section from 4 to 5 columns layout

--- app/Filament/Resources/ProductInTransitResource.php
+++ app/Filament/Resources/ProductInTransitResource.php
@@ -58,7 +58,7 @@ class ProductInTransitResource extends Resource
             ->schema([
                 Section::make('Основная информация')
                     ->schema([
-                        Grid::make(4)
+                        Grid::make(5)
                             ->schema([
                                 TextInput::make('shipping_location')
                                     ->label('Место отгрузки')
